d4896dafa537822dcda6d525b79209f73ce0a061
PouyaBirvand <pooyabirvand@gmail.com>
2025-08-08 00:30:00 +0330

    added new img

--- src/components/sections/home/HeroSection.tsx
+++ src/components/sections/home/HeroSection.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/Button'
 export default function HeroSection() {
   return (
     <section className="relative h-screen w-full">
-      <div className="absolute inset-0 z-0 bg-[url(/lux_pro_cart.webp)] bg-cover bg-center brightness-75" />
+      <div className="absolute inset-0 z-0 bg-[url(/Lux_Pro_cart.webp)] bg-cover bg-center brightness-75" />
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-white gap-8 text-center px-4">
         <h1 className="text-4xl md:text-6xl italic font-playfair tracking-wider">
           The Open Collection
